d928705d9fdfb64968ccf39ad6263cd6fefc2541
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2022-02-16 10:44:12 +0100

    chore: la være å bundle eksterne pakker annet enn nrk core components
    Markerer _alle_ pakker som external, unntatt NRK Core Components, så de ikke blir bundlet med i
    bygg.

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,9 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { babel } from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-import fs from "fs";
-import path from "path";
 
 const extensions = [".js", ".ts", ".tsx"];
 const outputFolder = "build";
-
-const allFremtindPackagesNames = getFremtindPackageNames();
 
 const defaultPlugins = [
     nodeResolve({ extensions }),
@@ -25,19 +21,17 @@ const uglifiedPlugins = [...defaultPlugins, terser({ ecma: "es5" })];
 function config(plugins) {
     return {
         input: "src/index.ts",
-
         plugins: plugins,
-        // Fremtind packages are marked as internal so that packages that depend on each other don't get inlined in each other
-        // @nrk packages should be marked as external once patches are no longer necessary (see https://github.com/fremtind/jokul/issues/1215)
-        external: [
-            "react",
-            "downshift",
-            "match-sorter",
-            "nanoid",
-            /* /@nrk\/core/, */ "classnames",
-            /@babel\/runtime/,
-            ...allFremtindPackagesNames,
-        ],
+        external: function (id) {
+            const externalPatterns = [/@fremtind\/jkl-/, /packages\/.*-react\/build/, /node_modules/];
+            // Any packages that are either a) patched by us, or b) small and used only by one or two packages
+            // should be excluded from the external-list so they are bundled on export
+            const internalPatterns = [/@nrk\/core/];
+            return (
+                externalPatterns.some((pattern) => pattern.test(id)) &&
+                !internalPatterns.some((pattern) => pattern.test(id))
+            );
+        },
     };
 }
 
@@ -52,23 +46,3 @@ export default [
     configWithOutput(config(uglifiedPlugins), { file: `${outputFolder}/esm/index.min.js`, format: "esm" }),
     configWithOutput(config(uglifiedPlugins), { file: `${outputFolder}/browser/index.min.js`, format: "esm" }),
 ];
-
-function getFremtindPackageNames() {
-    const ignore = [".DS_Store"];
-    const basePackagePath = path.resolve(__dirname, "packages");
-    const packagesFolderNames = fs.readdirSync(basePackagePath);
-
-    return packagesFolderNames
-        .filter((packageFolderName) => !ignore.includes(packageFolderName))
-        .map((packageFolderName) => {
-            try {
-                const packageJson = fs.readFileSync(path.resolve(basePackagePath, packageFolderName, "package.json"));
-                return JSON.parse(packageJson).name; // Return package name
-            } catch (e) {
-                // This is just a warning
-                console.log(`Failed to read package.json version for ${packageFolderName}.`);
-            }
-            return undefined;
-        })
-        .filter(Boolean); // Remove undefined in case of failing to find package.json
-}
